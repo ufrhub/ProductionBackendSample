67daf97d9dfadc0986f0e4c402d9d45e340da1fc
Umar Farooque <mdumarfarooqu@gmail.com>
2024-07-23 01:39:49 +0530

    Server Setup Done

--- Source/Controllers/TestControllers.js
+++ Source/Controllers/TestControllers.js
@@ -1,11 +1,38 @@
 export const TestGetRequest = (Request, Response) => {
-    setTimeout(() => {
-        Response.json(
-            [
-                {
-                    message: `App is running on port ${process.env.PORT}...!`, // Send a JSON response with a message
-                }
-            ]
-        );
-    }, 10000);
+    for (let i = 0; i < 10000000000; i++) {
+
+    }
+    Response.json(
+        [
+            {
+                message: `Worker ${process.pid} is handling the task...!`, // Send a JSON response with a message
+            }
+        ]
+    );
+}
+
+export const TestGetRequest2 = (Request, Response) => {
+    for (let i = 0; i < 30000000000; i++) {
+
+    }
+    Response.json(
+        [
+            {
+                message: `Worker ${process.pid} is handling the task 2...!`, // Send a JSON response with a message
+            }
+        ]
+    );
+}
+
+export const TestGetRequest3 = (Request, Response) => {
+    for (let i = 0; i < 50000000000; i++) {
+
+    }
+    Response.json(
+        [
+            {
+                message: `Worker ${process.pid} is handling the task 3...!`, // Send a JSON response with a message
+            }
+        ]
+    );
 }
--- Source/Routes/TestRouters.js
+++ Source/Routes/TestRouters.js
@@ -5,10 +5,12 @@ import EXPRESS from "express";
 const ROUTER = EXPRESS.Router();
 
 /********************* Import The Controllers *********************/
-import { TestGetRequest } from "../Controllers/TestControllers.js";
+import { TestGetRequest, TestGetRequest2, TestGetRequest3 } from "../Controllers/TestControllers.js";
 
 /********************* Declare The Routes And Bind With The Controller Methods *********************/
 ROUTER.get("/testGetRequest", TestGetRequest);
+ROUTER.get("/testGetRequest2", TestGetRequest2);
+ROUTER.get("/testGetRequest3", TestGetRequest3);
 
 /********************* Export The Router *********************/
 export default ROUTER;
